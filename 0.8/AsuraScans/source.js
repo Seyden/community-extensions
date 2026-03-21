9ea048c852182c0dba0f0ef2e43b5083b4811e91
Seyden <saiifii@live.de>
2026-03-21 13:35:05 +0000

    Deploying to gh-pages from @ Seyden/community-extensions@3fffdc5405904d190e168b10160b6580adf52056 🚀

--- 0.8/AsuraScans/source.js
+++ 0.8/AsuraScans/source.js
@@ -4907,8 +4907,8 @@ var _Sources = (() => {
           image: comic.coverUrl || source.fallbackImage,
           covers: [comic.coverUrl],
           status,
-          author: comic.author?.trim() || "Unknown",
-          artist: comic.artist?.trim() || "Unknown",
+          author: comic.author ? comic.author.trim() : "Unknown",
+          artist: comic.artist ? comic.artist.trim() : "Unknown",
           tags: tagSections,
           desc: description,
           rating: comic.rating
